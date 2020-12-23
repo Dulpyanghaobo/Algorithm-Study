b2feaa2abbb7a682a17db078c0aeb0cc8bfe6301
Dulpyanghaobo <2533382191@qq.com>
2020-12-23 10:25:17 +0800

    模拟栈

---
+++ mockStack/mockStack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mockStack
+//
+//  Created by yhb on 2020/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
