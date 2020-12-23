b60f805241597e005e438acdee1f87aac1a33b95
Dulpyanghaobo <2533382191@qq.com>
2020-12-23 10:25:31 +0800

    模拟栈

--- mockStack/mockStack/main.cpp
+++ mockStack/mockStack/main.cpp
@@ -6,7 +6,38 @@
 //
 
 #include <iostream>
-
+class Stack {
+//    目前存放的元素
+    int count = 0;
+//    栈的长度
+    int len;
+//    栈
+    int *p = new int [len];
+    
+public:
+    void push (int x) {
+        assert(!full());
+        p[count++] = x;
+    }
+    void pop (int x) {
+        assert(!empty());
+         x = p[--count];
+    }
+    bool full () {
+        if (count > len) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    bool empty () {
+        if (count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
